9f62d4b9a34ce788909284d58f27bd8924e4fce8
Imad <md.imad@gmail.com>
2017-11-23 19:30:18 +0100

    move helper functions and standalone utils to different files

---
+++ debs/utils/qsimulator.py
@@ -0,0 +1,31 @@
+"""Standalone file to Simulate streaming of triples from local file
+"""
+
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+observations_queue = 'debs_observations'
+channel.queue_declare(observations_queue, auto_delete=True)
+TERMINATION_MESSAGE="~~TERMINATION MESSAGE~~"
+
+observations_file = "/home/imad/workspace/agt-challenge/test_data/18.04.2017.1molding_machine_308dp/molding_machine_308dp.nt"
+
+def stream_observations(observations_file):
+    print ("Streaming observations")
+
+    with open(observations_file,'r') as fp:
+        for line in fp:
+            channel.basic_publish(exchange='',
+                                  routing_key=observations_queue,
+                                  body=line)
+            #sub, pred, obj = parser.parse_triple(line.strip('\n'))
+            #process_observations(sub, pred, obj)
+
+    channel.basic_publish(exchange='',
+                          routing_key=observations_queue,
+                          body=TERMINATION_MESSAGE)
+
+
+stream_observations(observations_file)
---
+++ debs/utils/utils.py
@@ -0,0 +1,47 @@
+from . import globals as global_vars
+
+def serialize_metadata():
+    """Pickle metadata for easy loading/unloading
+    """
+    print("Serializing Metadata ...")
+    import pickle
+    machine_datafile = 'machines_meta.pickle'
+    model_datafile = 'machine_models_meta.pickle'
+    print(f"Writing machine data to {machine_datafile}")
+    with open(machine_datafile, 'wb') as fp:
+        pickle.dump(global_vars.machines, fp)
+    print(f"Writing models data to {model_datafile}")
+    with open(model_datafile, 'wb') as fp:
+        pickle.dump(global_vars.machine_models, fp)
+    print("Finished")
+
+def csv_print():
+    """Write stateful properties into a CSV file
+    """
+    row_array = []
+    headers = []
+    header_set = False
+    for group in global_vars.event_map:
+        col_array = []
+        og = global_vars.event_map[group]
+        col_array.append(og.machine_id)
+        col_array.append(og.timestamp_id)
+        for ob in og.observations:
+            obs = og.observations[ob]
+            col_array.append(obs.output_value)
+            if not header_set:
+                headers.append(obs.observed_property)
+
+        if not header_set:
+            row_array.append(['', '', *headers])
+            header_set = True
+        
+        row_array.append(col_array)
+
+    import csv
+    with open('test_new.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        #writer.writerow(headers)
+        for row in row_array:
+            writer.writerow(row)
+    
