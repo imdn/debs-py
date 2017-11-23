277ba5a78e150fa0b1d874a75c61669ba831d1b8
Imad <md.imad@gmail.com>
2017-11-23 01:45:06 +0100

    use orderedDict for event window

--- debs/globals.py
+++ debs/globals.py
@@ -1,6 +1,7 @@
 """
 Global variables to share across files
 """
+from collections import OrderedDict
 
 # Required parameters W, N and M
 WINDOW_SIZE=10
@@ -15,11 +16,11 @@ machines = dict()
 machine_models = dict()
 
 # Keep track of events (observation groups) as they are streamed in
-event_map = dict()
+event_map = OrderedDict()
 
 # Filter list of properties to cluster. Useful for debugging
-#properties_to_filter = None
-properties_to_filter = ['_59_31']
+properties_to_filter = []
+#properties_to_filter = ['_59_31']
 
 def get_machine_property(machine_id, prop_id, prop_name):
     model_name = machines[machine_id].model
