3384411cc20f446595808b0dbd7c92143c1ab8bc
Imad <md.imad@gmail.com>
2017-11-24 01:05:11 +0100

    use Hobbit env vars; initialize connection in proc

--- debs/globals.py
+++ debs/globals.py
@@ -2,6 +2,7 @@
 Global variables to share across files
 """
 import pika
+import os
 from collections import OrderedDict
 from . import dispatcher
 
@@ -26,20 +27,26 @@ dispatcher = dispatcher.Dispatcher()
 properties_to_filter = []
 #properties_to_filter = ['_59_5']
 
-# RabbitMQ connection
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
-observations_queue = 'debs_observations'
-anomalies_queue = 'debs_anomalies'
-#channel.queue_declare(observations_queue, auto_delete=True)
-#channel.queue_declare(anomalies_queue, auto_delete=True)
-TERMINATION_MESSAGE="~~TERMINATION MESSAGE~~"
-
-
 def get_machine_property(machine_id, prop_id, prop_name):
     model_name = machines[machine_id].model
     properties = machine_models[model_name].properties[prop_id]
     return getattr(properties, prop_name)
+
+def init_connections():
+    """Intialize connections"""
+    global channel, connection, observations_queue, anomalies_queue, TERMINATION_MESSAGE
+
+    # Hobbit and RabbitMQ specific envvars
+    mq_hostname = os.environ['HOBBIT_RABBIT_HOST'] 
+    TERMINATION_MESSAGE="~~TERMINATION MESSAGE~~"
+    observations_queue = os.environ['DATA_GEN_2_SYSTEM_QUEUE_NAME']
+    anomalies_queue = os.environ['SYSTEM_2_EVAL_STORAGE_QUEUE_NAME']
+    
+    connection = pika.BlockingConnection(pika.ConnectionParameters(mq_hostname))
+    channel = connection.channel()
+    channel.queue_declare(observations_queue, auto_delete=True)
+    channel.queue_declare(anomalies_queue, auto_delete=True)
+
 
 def exit_gracefully():
     """Shutdown all connections etc. before terminating
