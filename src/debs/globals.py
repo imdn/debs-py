3f479a72969318ded50a61a6d879d24ad1b43fef
Imad <md.imad@gmail.com>
2017-11-27 16:19:04 +0100

    change env var names

--- src/debs/globals.py
+++ src/debs/globals.py
@@ -34,8 +34,8 @@ def get_machine_property(machine_id, prop_id, prop_name):
     return getattr(properties, prop_name)
 
 # HOBBIT Platform Specific
-mq_hostname = os.environ['RABBIT_MQ_HOST_NAME_KEY'] 
-session_id = os.environ['HOBBIT_SESSION_ID_KEY']
+mq_hostname = os.environ['HOBBIT_RABBIT_HOST'] 
+session_id = os.environ['HOBBIT_SESSION_ID']
 TERMINATION_MESSAGE="~~Termination Message~~"
 SYSTEM_READY_SIGNAL = b'\x01'
 TASK_GENERATION_FINISHED = b'\x0F'
@@ -60,8 +60,10 @@ def construct_cmd_queue_msg(signal):
 def unpack_cmd_queue_msg(msg):
     session_id_bytes = session_id.encode('utf-8')
     length = len(session_id_bytes)
+    payload_length = length + 5 # SessionId bytes + 4 length bytes + 1 cmd byte
+    trimmed_message = bytearray(msg)[0:payload_length]
     format_string = f">i{length}sc"
-    rcv_len, rcv_sid, cmd = struct.unpack(format_string, msg)
+    rcv_len, rcv_sid, cmd = struct.unpack(format_string, trimmed_message)
     return (rcv_len, rcv_sid, cmd)
 
 def send_to_cmd_queue(message):
@@ -81,10 +83,12 @@ def init_connections():
     input_channel = connection.channel()
     output_channel = connection.channel()
     cmd_channel = connection.channel()
+    input_channel.basic_qos(prefetch_count=1)
+    output_channel.basic_qos(prefetch_count=1)
     cmd_channel.exchange_declare(exchange=CMD_EXCHANGE,
-                                           exchange_type='fanout',
-                                           durable=False,
-                                           auto_delete=True)
+                                 exchange_type='fanout',
+                                 durable=False,
+                                 auto_delete=True)
     input_channel.queue_declare(input_queue, auto_delete=True)
     output_channel.queue_declare(output_queue, auto_delete=True)
 
